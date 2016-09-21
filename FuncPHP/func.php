704d1281d3d62da7ee7002a7e30f9287fb589cfa
Mitlond <comp122012016@mail.ru>
2016-09-21 16:50:39 +0300

    PHP

---
+++ FuncPHP/func.php
@@ -0,0 +1,53 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+<head>
+<title></title>
+</head>
+<body>
+<?php
+for ($i=0,$x=0; $x<16; $x++){
+for ($y=0; $y<16;$y++){
+	$chars[$x] [$y] = array($i,chr($i));
+	$i++;
+}	
+}
+?>
+<table border=1 cellpadding=1 cellspacing=0>
+<?foreach($chars as $row) { ?>
+<tr>
+<?foreach($row as $cell) {?>
+<td>
+<?=$cell[0] ?>:
+<b><tt><?=$cell[1]?></tt></b>
+</td>
+<?}?>
+</tr>
+<?}?>
+</table>
+<?php
+
+echo str_repeat("Elros", 10);
+?>
+ <?php
+
+$text = "\t\t I am fell sexy and free :)   ";
+
+echo trim($text);           
+echo trim($text, " \t."); 
+$clean = trim($binary, "\x00..\x1F");
+
+?> 
+ <?php
+$str = "\n";
+if (ord($str) == 10) {
+    echo "Первый символ \$str - перевод строки.\n";
+}
+?> 
+ <?php
+
+
+?> 
+
+</body>
+</html>
---
+++ FuncPHP/index.php
@@ -0,0 +1,76 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+<head>
+<title></title>
+</head>
+<body>
+<?php
+$str     = "Строка 1\nСтрока 2\rСтрока 3\r\nСтрока 4\n";
+$order   = array("\r\n", "\n", "\r");
+$replace = '<br />';
+echo $newstr = str_replace($order, $replace, $str);
+$search  = array('A', 'B', 'C', 'D', 'E');
+$replace = array('B', 'C', 'D', 'E', 'F');
+$subject = 'A';
+echo str_replace($search, $replace, $subject);
+$letters = array('я', 'о');
+$fruit   = array('яблоко', 'орех');
+$text    = 'я о';
+$output  = str_replace($letters, $fruit, $text);
+echo $output;
+?>
+
+ <?php
+$str = 'abcdef';
+echo strlen($str); 
+$str = ' ab cd ';
+echo strlen($str); 
+?> 
+
+<?php
+$mystring = 'abc';
+$findme   = 'a';
+$pos = strpos($mystring, $findme);
+
+if ($pos !== false) {
+     echo "Строка '$findme' найдена в строке '$mystring'";
+         echo " в позиции $pos";
+} else {
+     echo "Строка '$findme' не найдена в строке '$mystring'";
+}
+?>
+
+<?php
+    
+echo substr('abcdef', 1, 3);  // bcd
+echo substr('abcdef', 0, 8);  // abcdef
+echo substr('abcdef', -1, 1); // f
+
+$string = 'abcdef';
+echo $string[0];                 // a
+echo $string[3];              // d
+echo $string[strlen($string)-1]; // f
+?>
+
+<?php
+echo '<a href="mycgi?foo=', urlencode($userinput), '">';
+?>
+
+<?php
+$query = "my=apples&are=green+and+red";
+
+foreach (explode('&', $query) as $chunk) {
+    $param = explode("=", $chunk);
+
+    if ($param) {
+        printf("Value for parameter \"%s\" is \"%s\"<br/>\n", urldecode($param[0]), urldecode($param[1]));
+    }
+}
+?>
+ <?php
+$new = htmlspecialchars("<a href='test'>Test</a>", ENT_QUOTES);
+echo $new; // &lt;a href=&#039;test&#039;&gt;Test&lt;/a&gt;
+?> 
+</body>
+</html>
